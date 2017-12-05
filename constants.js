981f281deb07dca4f66be7b9225714ba7f16855f
makesh <mahesh.mmk48@gmail.com>
2017-12-05 15:33:36 +0530

    latest ngrok

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
-var EP_ENDPOINT = "http://4c00070e.ngrok.io";//"http://e416fccc.ngrok.io";
+var EP_ENDPOINT = "http://6f0ef5be.ngrok.io";//"http://e416fccc.ngrok.io";
 var EP_LOGIN = "/cortex/oauth2/tokens";
 var DEFAULT_ITEM = "alien_sku";
 var LOOK_UP = "/cortex/lookups/mobee/items?followlocation";
